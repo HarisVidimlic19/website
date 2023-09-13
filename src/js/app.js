a1f58e0225679b328a24e4525fa9859b85b9b94e
HarisVidimlic19 <hvidimlic1234@hotmail.com>
2023-09-13 18:29:30 -0400

    added css to webpack. maybe get fonts now?

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,67 @@
+// Import CSS
+import '../css/animate.css'
+import '../css/aos.css'
+import '../css/bootstrap.min.css'
+import '../css/flaticon.css'
+import '../css/icomoon.css'
+import '../css/ionicons.min.css'
+import '../css/magnific-popup.css'
+import '../css/open-iconic-bootstrap.min.css'
+import '../css/owl.carousel.min.css'
+import '../css/owl.theme.default.min.css'
+import '../css/style.css'
 
-AOS = require('./aos.js')
+// // Import Images
+// import '../images/20230618_162801.jpg'
+// import '../images/about.jpg'
+// import '../images/bg_1.jpg'
+// import '../images/bg_2.png'
+// import '../images/bg_3.jpg'
+// import '../images/project-1.png'
+// import '../images/project-2.png'
+// import '../images/project-3.png'
+// import '../images/project-4.png'
+// import '../images/project-5.png'
+// import '../images/project-6.png'
+
+// // Import Fonts
+// import "../fonts/flaticon/font/Flaticon.eot"
+// import "../fonts/flaticon/font/Flaticon.svg"
+// import "../fonts/flaticon/font/Flaticon.ttf" 
+// import "../fonts/flaticon/font/Flaticon.woff"
+// import "../fonts/flaticon/font/Flaticon.woff2"
+
+// import '../fonts/icomoon/icomoon.eot'
+// import '../fonts/icomoon/icomoon.svg'
+// import '../fonts/icomoon/icomoon.ttf'
+// import '../fonts/icomoon/icomoon.woff'
+
+// import "../fonts/ionicons/fonts/ionicons.eot"
+// import "../fonts/ionicons/fonts/ionicons.svg"
+// import "../fonts/ionicons/fonts/ionicons.ttf"
+// import "../fonts/ionicons/fonts/ionicons.woff"
+// import "../fonts/ionicons/fonts/ionicons.woff2"
+
+// import "../fonts/open-iconic/open-iconic.eot"
+// import "../fonts/open-iconic/open-iconic.svg"
+// import "../fonts/open-iconic/open-iconic.ttf"
+// import "../fonts/open-iconic/open-iconic.woff"
+
+// // Import Data
+// import '../data/data.json'
+// import '../data/Haris_Vidimlic_CV.pdf'
+// import '../data/horizons.json'
+// import '../data/output.html'
+// import '../data/earth.txt'
+// import '../data/jupiter.txt'
+// import '../data/mars.txt'
+// import '../data/mercury.txt'
+// import '../data/neptune.txt'
+// import '../data/saturn.txt'
+// import '../data/uranus.txt'
+// import '../data/venus.txt'
+
+// Import JS
 require('jquery-migrate')
 require('./jquery.easing.1.3.js')
 require('./jquery.stellar.min.js')
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 var webpack = require('webpack');
 var environment = process.env.NODE_ENV || 'development';
 var dev = environment !== 'production';
@@ -18,8 +19,38 @@ module.exports = {
             'window.$': 'jquery',
             'jQuery': 'jquery',
             'popper.js': 'popper'
-        })
+        }),
+        new MiniCssExtractPlugin(),
     ],
+    module: {
+        rules: [
+            {
+                test: /\.(css)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: () => [
+                                    require('autoprefixer')
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
+    }
 
 
 };
